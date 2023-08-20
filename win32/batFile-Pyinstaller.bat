9257bdc90ae0322c8685c7176f668840d7bb65e1
Soorya U <sooryau7@gmail.com>
2023-08-20 19:54:58 +0530

    Adding Fonts and Updates - v1.0.1

--- win32/batFile-Pyinstaller.bat
+++ win32/batFile-Pyinstaller.bat
@@ -3,6 +3,8 @@ pyinstaller  ^
     --onefile ^
     --add-data "./Assets/wonderwords;wonderwords" ^
     --add-data "./Images;Images" ^
+    --add-data "./Fonts;Fonts" ^
     --name "win32_app" ^
     --distpath "./win32" ^
     'main.py'
+
