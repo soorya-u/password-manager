3e46f20ebb3474a6d8eb8f5037b02a5c2d05b57d
Soorya U <sooryau7@gmail.com>
2023-11-15 18:46:37 +0530

    Updated Database

--- Modules/Database.py
+++ Modules/Database.py
@@ -10,7 +10,8 @@ class Database:
     # * Return Value: Connection
     @classmethod
     def _sqliteConnection(cls):
-        conn = sqlite3.connect(cls.app_data+r'/zAsh7/Password Manager/.database/database.db')
+        conn = sqlite3.connect(
+            cls.app_data+r'/zAsh7/Password Manager/.database/database.db')
         return conn
 
     # * Function to Create userTable
@@ -26,14 +27,9 @@ class Database:
 
             conn = Database._sqliteConnection()
             c = conn.cursor()
-            c.execute('''
-                        create table userTable (
-                            first_name varchar(255),
-                            master_user_name varchar(255),
-                            hashed_password varchar(255),
-                            unique_key varchar(255)
-                        );
-                ''')
+            with open(r'./SQL/create-userTable.sql', 'r') as f:
+                query = f.read()
+                c.execute(query)
             conn.close()
 
     # * Function to insert User and create a Account Table
@@ -43,20 +39,20 @@ class Database:
     def userInsertion(cls, first_name: str, master_user_name: str, hashed_password: str, unique_key: str) -> None:
         conn = Database._sqliteConnection()
         c = conn.cursor()
-        c.execute(f'''
-                    insert into userTable values ("{first_name}", "{master_user_name}", "{hashed_password}", "{unique_key}");
-                ''')
+        with open(r'./SQL/insert-userTable.sql', 'r') as f:
+            params = {
+                'first_name': first_name,
+                'master_user_name': master_user_name,
+                'hashed_password': hashed_password,
+                'unique_key': unique_key
+            }
+            query = f.read()
+            c.execute(query, params)
         conn.commit()
 
-        c.execute(f'''
-                    create table {master_user_name} (
-                        platform varchar(255),
-                        url varchar(255),
-                        email varchar(255),
-                        user_name varchar(255),
-                        encryted_password varchar(255)
-                    );
-            ''')
+        with open(r'./SQL/create-userData.sql', 'r') as f:
+            query = f.read().format(master_user_name)
+            c.execute(query)
         conn.close()
 
     # * Function to insert Account
@@ -66,9 +62,16 @@ class Database:
     def accountInsertion(cls, master_user_name: str, platform: str, url: str, email: str, user_name: str, encrypted_password: str) -> None:
         conn = Database._sqliteConnection()
         c = conn.cursor()
-        c.execute(f'''
-                    insert into {master_user_name} values ("{platform}", "{url}", "{email}", "{user_name}", "{encrypted_password}");
-            ''')
+        params = {
+            'platform': platform,
+            'url': url,
+            'email': email,
+            'user_name': user_name,
+            'encrypted_password': encrypted_password
+        }
+        with open(r'./SQL/insert-userData.sql', 'r') as f:
+            query = f.read().format(master_user_name)
+            c.execute(query, params)
         conn.commit()
         conn.close()
 
@@ -79,12 +82,17 @@ class Database:
     def getUserHashedPassword(cls, master_user_name: str) -> str:
         conn = Database._sqliteConnection()
         c = conn.cursor()
-        data = list(c.execute(
-            f'''select hashed_password from userTable where master_user_name="{master_user_name}"''').fetchall()[0])
+        params = {
+            'master_user_name': master_user_name
+        }
+        with open(r'./SQL/read-HP-userTable.sql', 'r') as f:
+            query = f.read()
+            c.execute(query, params).fetchall()
+            data = list(c.execute(query, params).fetchall()[0])
         conn.commit()
         conn.close()
         return data.pop()
-    
+
     # * Function to get User Key
     # * Parameters: Master Username
     # * Return Value: Unique Key -> String
@@ -92,12 +100,16 @@ class Database:
     def getUserUniqueKey(cls, master_user_name: str) -> str:
         conn = Database._sqliteConnection()
         c = conn.cursor()
-        data = list(c.execute(
-            f'''select unique_key from userTable where master_user_name="{master_user_name}"''').fetchall()[0])
+        params = {
+            'master_user_name': master_user_name
+        }
+        with open(r'./SQL/read-UK-userTable.sql', 'r') as f:
+            query = f.read()
+            data = list(c.execute(query, params).fetchall()[0])
         conn.commit()
         conn.close()
         return data.pop()
-    
+
     # * Function to get User First Name
     # * Parameters: Master Username
     # * Return Value: First Name -> String
@@ -105,8 +117,12 @@ class Database:
     def getUserFirstName(cls, master_user_name: str) -> str:
         conn = Database._sqliteConnection()
         c = conn.cursor()
-        data = list(c.execute(
-            f'''select first_name from userTable where master_user_name="{master_user_name}"''').fetchall()[0])
+        params = {
+            'master_user_name': master_user_name
+        }
+        with open(r'./SQL/read-FN-userTable.sql', 'r') as f:
+            query = f.read()
+            data = list(c.execute(query, params).fetchall()[0])
         conn.commit()
         conn.close()
         return data.pop()
@@ -115,15 +131,17 @@ class Database:
     # * Parameters: Master Username
     # * Return Value: All Account List -> List
     @classmethod
-    def getAccountTable(cls, master_user_name: str) ->list[list[str]]:
+    def getAccountTable(cls, master_user_name: str) -> list[list[str]]:
         conn = Database._sqliteConnection()
         c = conn.cursor()
-        data = c.execute(f'''select * from {master_user_name}''').fetchall()
-        
+        with open(r'./SQL/read-userData.sql', 'r') as f:
+            query = f.read().format(f'"{master_user_name}"')
+            data = c.execute(query).fetchall()
+
         for i in range(len(data)):
             list_cell = list(data[i])
             data.pop(i)
-            data.insert(i,list_cell)
+            data.insert(i, list_cell)
 
         conn.commit()
         conn.close()
